48d8b46372fb09c939cb5c7e1e8da09d6802e52d
Md Sofian Hasan <Dip.akand9899@gmail.com>
2026-07-25 00:06:34 +0600

    create new branch module-09 and pull request

---
+++ loop.js
@@ -0,0 +1,54 @@
+/**
+ *
+ * A loop is a programming tool that repeats a block of code multiple times
+ * - so you didn't have to write it over and over again.
+ * - Same action happens over and over until you decide to stop.
+ *
+ * - To repeat a task multiple times without writing duplicate code.
+ *
+ * 1. starting situation
+ * 2. count value adapt
+ * 3. Stopping condition (until ends or fixed number of times)
+ * 4. repetitive same or similar task
+ *
+ *
+ */
+
+// const count = 0;
+// let count = 0;
+// count < 10;
+// count++;
+// console.log("do something");
+
+// for (initialization; condition; modification / value_adjustment) {
+//   // repetitive tasks
+// }
+
+// for (start; end; step or increment/decrement) {}
+
+// for (variable; condition; update) {}
+
+/**
+ * for (expression 1; expression 2; expression 3) {}
+ *
+ * expression 1 is executed one time before the execution of the code block.
+ *
+ * expression 2 defines the condition for executing the code block
+ *
+ * expression 3 is executed every time the code block has been executed.
+ *
+ */
+
+/**
+ *
+ * for (start; end; step) {}
+ *
+ * - Declare a variable and gives him value is called start.
+ * - When it is stop is called end.
+ * - How many times it perform is called step.
+ *
+ */
+
+for (let count = 0; count < 5; count++) {
+  console.log("do something");
+}
---
+++ repeat.js
@@ -0,0 +1,22 @@
+// Here, ...repeat manually
+console.log("Show me something");
+console.log("Show me something");
+console.log("Show me something");
+console.log("Show me something");
+console.log("Show me something");
+console.log("Show me something");
+console.log("Show me something");
+console.log("Show me something");
+console.log("Show me something");
+console.log("Show me something");
+
+console.log();
+
+// To repeat a task multiple times without writing duplicate code.
+// short, clean, and scale to 10 or 10 million
+for (let i = 0; i < 10; i++) {
+  console.log("Show me something");
+}
+
+// Repetition means doing the same action again and again. We do it every day without even noticing.
+// Same action happens over and over until you decide to stop.
