8590a1b351fcbc405b7466311cf6682a3c3eb78f
Md Sofian Hasan <Dip.akand9899@gmail.com>
2026-07-25 05:34:04 +0600

    Update repeat.js

--- repeat.js
+++ repeat.js
@@ -13,13 +13,13 @@ console.log("Show me something");
 console.log();
 
 // To repeat a task multiple times without writing duplicate code.
-// short, clean, and scale to 10 or 10 million
+// short, clean, and scalable to 10 or 10 million
 for (let i = 0; i < 10; i++) {
   console.log("Show me something");
 }
 
 // Repetition means doing the same action again and again. We do it every day without even noticing.
-// Same action happens over and over until you decide to stop.
+//The same action happens over and over until you decide to stop.
 
 /**
  *
@@ -36,6 +36,6 @@ for (let i = 0; i < 10; i++) {
  * - The loop may run infinitely
  *
  * What is the difference between break and continue?
- * - Break exits the loop, continue skips the current iteration
+ * - Break exits the loop; continue skips the current iteration
  *
  * */
